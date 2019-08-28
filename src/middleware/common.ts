be995a51b09cd92ad0163ab056315b63e07319d2
sanath <ramsaanath@gmail.com>
2019-08-28 22:55:06 +0530

    Added comments

--- src/middleware/common.ts
+++ src/middleware/common.ts
@@ -1,38 +1,61 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { validationResult, ValidationChain } from 'express-validator';
+import config from '../config';
 import HttpError from '../errors/http-error';
 import cors from 'cors';
 import parser from 'body-parser';
 import morgan from 'morgan';
 
-// CORS configuration
+/**
+ * Adds a middleware to handle Cross-origin resource sharing
+ *
+ * @param router
+ */
 export const handleCors = (router: Router) =>
     router.use(cors({ credentials: true, origin: true }));
 
-// Parse incoming request bodies before handling, available under the req.body property.
+/**
+ * Adds a middleware to [arse incoming request bodies
+ * before handling, available under the req.body property.
+ *
+ * @param router
+ */
 export const parseRequestBody = (router: Router) => {
     router.use(parser.urlencoded({ extended: true }));
     router.use(parser.json());
 };
 
-// Log all incoming request
+/**
+ * Adds a middleware to log all incoming request
+ *
+ * @param route
+ */
 export const logRequest = (router: Router) => {
-    const { NODE_ENV } = process.env;
-    const format = NODE_ENV === 'production' ? 'tiny' : 'dev';
+    const format = config.isProduction ? 'tiny' : 'dev';
     router.use(morgan(format));
 };
 
+/**
+ * Validates the incoming request.
+ *
+ * If all validations are passed proceeds to the next middlewate in chain
+ * otherwise throws an HttpError.
+ *
+ * @param validations The list of conditions to validate
+ *
+ * @returns A Middleware that will handle the validation
+ */
 export const validate = (...validations: ValidationChain[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         await Promise.all(validations.map(validation => validation.run(req)));
         const errors = validationResult(req);
+
         if (errors.isEmpty()) {
             return next();
         }
-        const error = new HttpError(
-            422,
-            'Required fields are missing or invalid. Please refer the API docs'
-        );
+
+        const message = 'Required fields are missing or invalid.';
+        const error = new HttpError(422, message);
         next(error);
     };
 };
